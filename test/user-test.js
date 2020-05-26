3dedec062b2c859dd7c0ae4c59a46ca50db760b2
emontealvo <edwin.montealvo@gmail.com>
2020-05-26 16:10:12 -0600

    Delete unnecessay user data add test to user class

--- test/user-test.js
+++ test/user-test.js
@@ -2,8 +2,10 @@ import { expect } from 'chai';
 
 import User from '../src/user';
 import users from '../src/data/users-data';
+import recipeData from '../src/data/recipe-data'
+import RecipeRepo from '../src/recipeRepo'
 
-describe.skip('User', function() {
+describe('User', function() {
   let user;
   let userInfo;
   let recipe;
@@ -12,7 +14,7 @@ describe.skip('User', function() {
     userInfo = users[0];
     user = new User(userInfo)
 
-    recipe = {name: 'Chicken Parm', type: ['italian', 'dinner']};
+    recipe = recipeData[0];
   });
 
   it('should be a function', function() {
@@ -27,30 +29,48 @@ describe.skip('User', function() {
     expect(user.name).to.eq('Saige O\'Kon');
   });
 
-  it('should initialize with a pantry', function() {
+  it.skip('should initialize with a pantry', function() {
     expect(user.pantry[0].ingredient).to.eq(11477);
   });
 
-  it('should initialize with an empty favoriteRecipes array', function() {
-    expect(user.favoriteRecipes).to.deep.equal([]);
+  it('should check if recipe can be cooked', function() {
+    expect(user2.checkAbility2Cook(recipe)).to.be.true
   });
 
-  it('should initialize with an empty recipesToCook array', function() {
-    expect(user.recipesToCook).to.deep.equal([]);
+  it('should check if recipe can be cooked', function() {
+    let user2 = new User(users[1]);
+    expect(user2.checkAbility2Cook(recipe)).to.be.false
   });
 
-  it('should be able to save a recipe to favoriteRecipes', function() {
-    user.saveRecipe(recipe);
-    expect(user.favoriteRecipes[0].name).to.equal('Chicken Parm');
+  it('should be able to cook a recipe', function() {
+    user.cookRecipe(recipe);
+    expect(user.pantry[0].amount).to.equal(2.5);
+    expect(user.pantry[4].amount).to.equal(0);
+    expect(user.pantry[12].amount).to.equal(1);
   });
 
-  it('should be able to decide to cook a recipe', function() {
-    user.decideToCook(recipe);
-    expect(user.recipesToCook[0].name).to.equal('Chicken Parm');
+  it('should return the required ingredients if user cannot cook recipe', function() {
+    let user2 = new User(users[1]);
+    let requiredIngredients =   [
+      {name: 'egg', id: 1123, requiredQuantity: 1},
+      {name: 'granulated sugar', id: 19335, requiredQuantity: 0.5 },
+      {name: 'instant vanilla pudding mix', id: 19206, requiredQuantity: 2},
+      {name: 'light brown sugar', id: 19334, requiredQuantity: 0.5 },
+      {name: 'salt', id: 2047, requiredQuantity: 0.5}
+    ]
+    expect(user2.findRequiredIngredients(recipe)).to.deep.equal(requiredIngredients);
   });
 
-  it('should be able to filter recipes by type', function() {
-    user.saveRecipe(recipe);
-    expect(user.filterRecipes('italian')).to.deep.equal([recipe]);
+  it('should be able to find a recipe from a designated recipe repo', function() {
+    let recipeRepo = new RecipeRepo(recipeData);
+    let randomRecipe = recipeData[Math.floor(Math.random() * users.length)];
+    let foundRecipe = user.findRecipe(recipeRepo.recipes, randomRecipe);
+    expect(foundRecipe).to.deep.equal(randomRecipe);
+  });
+
+  it('should return a message when recipe is not found', function() {
+    let randomRecipe = recipeData[Math.floor(Math.random() * users.length)];
+    let foundRecipe = user.findRecipe(recipeRepo.favoriteRecipes, randomRecipe);
+    expect(foundRecipe).to.equal('Sorry, recipe not found');
   });
 });
