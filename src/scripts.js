3ee9c8e91fbcea4f2c476f1316ba45e4b265f0d1
carflor <cfdeclet@gmail.com>
2020-06-02 08:45:16 -0600

    Removed unused css style sheet tag

--- src/scripts.js
+++ src/scripts.js
@@ -200,11 +200,9 @@ function postIngredient(user) {
       .then(response => findUser(response.wcUsersData))
       .then(response => findPantryInfo(response))
       .catch(error => console.log(error))
-
     document.forms[0].reset();
   }
 }
-
 
 fetchData()
 
